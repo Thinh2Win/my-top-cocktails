ce14f03ac4e9f25a8ce32f47f430086486aa377a
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-03-29 17:21:33 -0700

    setting up folder structure for Next/Redux project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  aliases: {
+    components: path.join(__dirname, 'src/components'),
+  }
 }
 
 module.exports = nextConfig
