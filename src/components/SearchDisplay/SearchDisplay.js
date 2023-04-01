e46187db9e851fb4ce1c1672b35fac3e895b34e1
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-01 14:53:05 -0700

    added table component

--- src/components/SearchDisplay/SearchDisplay.js
+++ src/components/SearchDisplay/SearchDisplay.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function SearchDisplay() {
   return (
+    /*
+    create List table to hold ingredients
+    need table to determine whether we are excluding or including listed ingredients
+    display area should show recipe card in full detail when a recipe from list is clicked
+    */
     <>
       <div>This is the search display</div>
     </>
