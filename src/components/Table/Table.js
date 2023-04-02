635a2b6993aeca9c22ffcf1814174aabaee7ab47
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-02 12:40:03 -0700

    implemented Provider wrapper

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './Table.module.css';
 
 export default function Table() {
+  const ingredients = useSelector(state => state.ingredientsList);
+  // button and input to add to list
+  // list should be managed by state within store
+  /*
+eventually users will be able to click on a recipe
+and see missing ingredients. should have a button that
+adds missing ingredients into a cart
+  */
   return (
     <div className={styles.container}>
       <ul className={styles.list}>
---
+++ src/store/reducers/index.js
@@ -0,0 +1,8 @@
+
+import { combineReducers } from 'redux'
+import ingredientsList from './ingredientsList.js'
+
+export default combineReducers({
+  ingredientsList,
+
+});
