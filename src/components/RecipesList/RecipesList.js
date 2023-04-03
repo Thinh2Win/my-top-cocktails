522dc39c452e3fca3af5e1dd65b5b990fb066b91
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-02 21:08:43 -0700

    created recipe list component

---
+++ src/components/RecipesList/RecipesList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './RecipesList.module.css';
+import { useSelector } from 'react-redux';
+
+export default function RecipesList() {
+  return (
+    <div className={styles.container}>
+      RecipesList
+    </div>
+  )
+}
