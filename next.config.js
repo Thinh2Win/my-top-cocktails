ca2e36b2cfad380f29b730fb8430972646b3e47f
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-04-04 18:34:48 -0700

    configured images to be available from all domains

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
-  }
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
